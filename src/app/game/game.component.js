6b74ee85df3608ff3358c280f32d61d66fb27b70
Sachin <sachin.phatak92@gmail.com>
2017-08-10 02:02:10 +0530

    tile movement

--- src/app/game/game.component.js
+++ src/app/game/game.component.js
@@ -11,49 +11,38 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var GameComponent = (function () {
     function GameComponent() {
-        this.gridSize = [4, 4];
-        this.grid = [
-            [{}, {}, {}, {}],
-            [{}, {}, {}, {}],
-            [{}, {}, {}, {}],
-            [{}, {}, {}, {}]
-        ];
         this.tiles = [
-            [, 2, , 4],
-            [8, , ,],
-            [, , , 8],
-            [, , ,]
+            { row: 1, col: 2, num: 2 },
+            { row: 1, col: 4, num: 4 },
+            { row: 2, col: 1, num: 8 },
+            { row: 3, col: 4, num: 8 }
         ];
-        // moveUp(): void {
-        //     for (var j = 0; j < this.gridSize[1]; ++j) {
-        //         var numEmpty: number = 0;
-        //         for (var i = this.gridSize[0]-1; j >= 0; --j) {
-        //             if (!this.tiles[i][j]) {
-        //                 ++numEmpty;
-        //             } else {
-        //                 if (i !== this.gridSize[1]) {
-        //                     this.tiles[i].splice(j, numEmpty);
-        //                     j += numEmpty;
-        //                     while (numEmpty > 0) {
-        //                         this.tiles[i].unshift(undefined);
-        //                         --numEmpty;
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+        this.showTranspose = false;
+        this.gridSize = [4, 4];
+        this.grid = this.makeGrid();
     }
     GameComponent.prototype.makeGrid = function () {
-        var row;
-        var grid;
-        for (var i = 0; i < this.gridSize[1]; ++i) {
+        var row = [];
+        var grid = [];
+        for (var j = 0; j < this.gridSize[1]; ++j) {
             row.push({});
         }
-        for (var j = 0; j < this.gridSize[0]; ++j) {
+        for (var i = 0; i < this.gridSize[0]; ++i) {
             grid.push(row);
         }
         return grid;
+    };
+    GameComponent.prototype.getTransposedTiles = function (tiles) {
+        var transposedGrid = [];
+        for (var i = 0; i < tiles[0].length; ++i) {
+            transposedGrid.push([]);
+        }
+        for (var i = 0; i < tiles[0].length; ++i) {
+            for (var j = 0; j < tiles.length; ++j) {
+                transposedGrid[i][j] = tiles[j][i];
+            }
+        }
+        return transposedGrid;
     };
     GameComponent.prototype.keypress = function (e) {
         if (e.key === 'ArrowRight') {
@@ -62,46 +51,167 @@ var GameComponent = (function () {
         else if (e.key === 'ArrowLeft') {
             this.moveLeft();
         }
+        else if (e.key === 'ArrowUp') {
+            this.moveUp();
+        }
+        else if (e.key === 'ArrowDown') {
+            this.moveDown();
+        }
+    };
+    GameComponent.prototype.toggleTranspose = function (horizontalOrVertical) {
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            if (horizontalOrVertical === 'horizontal') {
+                if (_this.showTranspose) {
+                    _this.showTranspose = false;
+                    _this.tiles = _this.getTransposedTiles(_this.tiles);
+                    resolve();
+                }
+                else {
+                    resolve();
+                }
+            }
+            else if (horizontalOrVertical === 'vertical') {
+                if (_this.showTranspose) {
+                    resolve();
+                }
+                else {
+                    _this.showTranspose = true;
+                    _this.tiles = _this.getTransposedTiles(_this.tiles);
+                    resolve();
+                }
+            }
+        });
     };
     GameComponent.prototype.moveRight = function () {
-        for (var i = 0; i < this.gridSize[0]; ++i) {
-            var numEmpty = 0;
-            for (var j = this.gridSize[1] - 1; j >= 0; --j) {
-                if (!this.tiles[i][j]) {
-                    ++numEmpty;
-                }
-                else {
-                    if (j !== this.gridSize[1]) {
-                        this.tiles[i].splice(j + 1, numEmpty);
-                        j += numEmpty;
-                        while (numEmpty > 0) {
-                            this.tiles[i].unshift(undefined);
-                            --numEmpty;
-                        }
-                    }
+        var colMax = this.gridSize[1];
+        var tiles = this.tiles;
+        var sortOnCol = function (a, b) {
+            if (a.col < b.col) {
+                return -1;
+            }
+            else if (a.col > b.col) {
+                return 1;
+            }
+            return 0;
+        };
+        var rowGroups = [];
+        this.tiles.forEach(function (tile, index) {
+            if (!rowGroups[tile.row]) {
+                rowGroups[tile.row] = [];
+            }
+            rowGroups[tile.row].push({
+                col: tile.col,
+                index: index
+            });
+        });
+        rowGroups.forEach(function (rowGroup) {
+            if (rowGroup) {
+                rowGroup.sort(sortOnCol);
+                var colPushPosition = colMax;
+                for (var i = rowGroup.length - 1; i >= 0; --i) {
+                    tiles[rowGroup[i].index].col = colPushPosition;
+                    --colPushPosition;
                 }
             }
-        }
+        });
     };
     GameComponent.prototype.moveLeft = function () {
-        for (var i = 0; i < this.gridSize[0]; ++i) {
-            var numEmpty = 0;
-            for (var j = 0; j < this.gridSize[1]; ++j) {
-                if (!this.tiles[i][j]) {
-                    ++numEmpty;
-                }
-                else {
-                    if (j !== 0) {
-                        this.tiles[i].splice(j - numEmpty, numEmpty);
-                        j -= numEmpty;
-                        while (numEmpty > 0) {
-                            this.tiles[i].splice(this.gridSize[1] - 1, undefined);
-                            --numEmpty;
-                        }
-                    }
+        var tiles = this.tiles;
+        var sortOnCol = function (a, b) {
+            if (a.col < b.col) {
+                return -1;
+            }
+            else if (a.col > b.col) {
+                return 1;
+            }
+            return 0;
+        };
+        var rowGroups = [];
+        this.tiles.forEach(function (tile, index) {
+            if (!rowGroups[tile.row]) {
+                rowGroups[tile.row] = [];
+            }
+            rowGroups[tile.row].push({
+                col: tile.col,
+                row: tile.row,
+                index: index
+            });
+        });
+        rowGroups.forEach(function (rowGroup) {
+            if (rowGroup) {
+                rowGroup.sort(sortOnCol);
+                var colPushPosition = 0;
+                for (var i = 0; i < rowGroup.length; ++i) {
+                    tiles[rowGroup[i].index].col = i + 1;
                 }
             }
-        }
+        });
+    };
+    GameComponent.prototype.moveUp = function () {
+        var tiles = this.tiles;
+        var sortOnRow = function (a, b) {
+            if (a.row < b.row) {
+                return -1;
+            }
+            else if (a.row > b.row) {
+                return 1;
+            }
+            return 0;
+        };
+        var colGroups = [];
+        this.tiles.forEach(function (tile, index) {
+            if (!colGroups[tile.col]) {
+                colGroups[tile.col] = [];
+            }
+            colGroups[tile.col].push({
+                col: tile.col,
+                row: tile.row,
+                index: index
+            });
+        });
+        colGroups.forEach(function (colGroup) {
+            if (colGroup) {
+                colGroup.sort(sortOnRow);
+                var rowPushPosition = 0;
+                for (var i = 0; i < colGroup.length; ++i) {
+                    tiles[colGroup[i].index].row = i + 1;
+                }
+            }
+        });
+    };
+    GameComponent.prototype.moveDown = function () {
+        var rowMax = this.gridSize[0];
+        var tiles = this.tiles;
+        var sortOnRow = function (a, b) {
+            if (a.row < b.row) {
+                return -1;
+            }
+            else if (a.row > b.row) {
+                return 1;
+            }
+            return 0;
+        };
+        var colGroups = [];
+        this.tiles.forEach(function (tile, index) {
+            if (!colGroups[tile.col]) {
+                colGroups[tile.col] = [];
+            }
+            colGroups[tile.col].push({
+                row: tile.row,
+                index: index
+            });
+        });
+        colGroups.forEach(function (colGroup) {
+            if (colGroup) {
+                colGroup.sort(sortOnRow);
+                var rowPushPosition = rowMax;
+                for (var i = colGroup.length - 1; i >= 0; --i) {
+                    tiles[colGroup[i].index].row = rowPushPosition;
+                    --rowPushPosition;
+                }
+            }
+        });
     };
     return GameComponent;
 }());
@@ -115,7 +225,8 @@ GameComponent = __decorate([
     core_1.Component({
         selector: 'game',
         templateUrl: './game.component.html'
-    })
+    }),
+    __metadata("design:paramtypes", [])
 ], GameComponent);
 exports.GameComponent = GameComponent;
 ;
